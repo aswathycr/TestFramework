383038c2027d643b50e29a9796983a9f64ffa502
Aswathy C R <aswathycr.c@ivedix.com>
2017-01-16 21:13:54 +0530

    test method

---
+++ TestFramework/Testing.swift
@@ -0,0 +1,20 @@
+//
+//  Testing.swift
+//  TestFramework
+//
+//  Created by Aswathy C R on 16/01/17.
+//  Copyright © 2017 Cabot. All rights reserved.
+//
+
+import Foundation
+
+public class Testing {
+    
+    public init() {
+        
+    }
+    
+    public func getTestMessage() -> String {
+        return "Test message!!!"
+    }
+}
